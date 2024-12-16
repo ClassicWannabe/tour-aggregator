5f24d4091e39a22f2835791789d60eec8091304a
ilyas.bekpayev <ilyas.bekpayev@gmail.com>
2024-12-16 11:46:12 +0500

    feat: header bg logic

--- frontend/src/components/Header/DesktopHeader.tsx
+++ frontend/src/components/Header/DesktopHeader.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import Logo from "@/components/Logo"
 import Typography from "@/components/ui/Typography"
 import { useTranslations } from "next-intl"
+import { cn } from "@/lib/utils"
 
 type Props = {
   background: "none" | "white"
@@ -12,17 +13,17 @@ const DesktopHeader: React.FC<Props> = ({ background }) => {
   const t = useTranslations("Shared")
 
   return (
-    <div className="px-[120px] py-4 flex items-center justify-between">
+    <div className={cn("px-[120px] py-4 flex items-center justify-between", `bg-${background}`)}>
       <Logo />
       <div className="flex items-center gap-6">
         <Typography variant="body2" color="white" as="p">
           {t("toTravellers")}
         </Typography>
         <Typography variant="body2" color="white" as="p">
-          {t("toTravellers")}
+          {t("toPartners")}
         </Typography>
         <Typography variant="body2" color="white" as="p">
-          {t("toTravellers")}
+          {t("support")}
         </Typography>
       </div>
     </div>
