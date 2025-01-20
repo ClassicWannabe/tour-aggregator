ac2c508596b1c7c1a0a6e45bf55b61a38ef00e80
ilyas.bekpayev <ilyas.bekpayev@gmail.com>
2025-01-20 10:53:03 +0500

    category page

--- client/src/features/header/desktop-header.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@/shared/ui/components/typography";
-
-export function DesktopHeader() {
-  return (
-    <header className="bg-transparent">
-      <Typography variant="body1">Desktop Header</Typography>
-    </header>
-  );
-}
--- client/src/shared/utils/device.ts
+++
@@ -1,8 +0,0 @@
-import { headers } from "next/headers";
-import { getSelectorsByUserAgent } from "react-device-detect";
-
-export const isMobileDevice = (): boolean => {
-  const userAgent = headers().get("user-agent") ?? "";
-  const { isMobile } = getSelectorsByUserAgent(userAgent);
-  return isMobile;
-};
